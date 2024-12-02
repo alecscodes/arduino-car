694dd93f735a2088eb5f18f3b2640c71f4340fba
alexx <alecscodes@users.noreply.github.com>
2024-12-02 18:05:05 +0100

    Refactor main.cpp to integrate Robot class and simplify setup and loop logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,14 @@
 #include <Arduino.h>
-#include "config.h"
-#include "sensors.h"
-#include "motors.h"
-#include "navigation.h"
+#include "robot.h"
 
-Motors motors(PinConfig::A_1B, PinConfig::A_1A, PinConfig::B_1B, PinConfig::B_1A);
-SensorSystem sensors(PinConfig::ECHO_PIN, PinConfig::TRIG_PIN,
-                     PinConfig::LEFT_IR, PinConfig::RIGHT_IR);
-RobotConfig robotConfig;
-Navigation navigation(motors, robotConfig);
+Robot robot;
 
 void setup()
 {
-  Serial.begin(9600);
-  motors.init();
-  sensors.init();
+  robot.init();
 }
 
 void loop()
 {
-  SensorData sensorData = sensors.read();
-  navigation.navigate(sensorData);
-  delay(RobotConfig::SENSOR_DELAY);
+  robot.update();
 }
